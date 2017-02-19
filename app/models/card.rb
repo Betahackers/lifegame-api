66f08105f1c0b1da4d13703ec680795e9e385d0a
Bernard Schembri <bernard@webgenix.com>
2017-02-19 15:56:29 +0100

    Support for age restriction

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,4 +4,13 @@ class Card < ApplicationRecord
   def self.shuffle(limit = 50)
     Card.all.order("RANDOM()").limit(limit)
   end
+
+  def self.pick_random()
+    Card.uncached { shuffle(1).first }
+  end
+
+  def constrained_by_age?(age)
+    return false if age_constraint.blank?
+    age_constraint < 0 ? age > age_constraint.abs : age < age_constraint
+  end
 end
