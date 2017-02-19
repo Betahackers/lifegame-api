b444ce8b7b8d140b6ca772df17c32436f0f48e58
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2017-02-19 16:36:52 +0100

    move card picking to model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,12 +1,48 @@
 class Card < ApplicationRecord
+  DEFAULT_START_AGE = 15
+  DEFAULT_AGE_INCREMENT = 1
+
   has_many :answers, dependent: :destroy
 
-  def self.shuffle(limit = 50)
-    Card.all.includes(answers: :points).order("RANDOM()").limit(limit)
-  end
+  def self.ordered(limit)
+    cards = Card.all.includes(answers: :points).all
 
-  def self.pick_random()
-    Card.uncached { shuffle(1).first }
+    card_deck = []
+
+    safety_max_limit = 1000
+    pick_count = 0
+
+    current_age = DEFAULT_START_AGE
+
+    while card_deck.size < [limit.to_i, cards.size].min && pick_count < safety_max_limit do
+      pick_count += 1
+      pick = cards.sample
+
+      Rails.logger.debug "Current age #{current_age}"
+      Rails.logger.debug "Picked card #{pick.id} with age_constraint: '#{pick.age_constraint}'"
+
+      # ignore card if it does not fit age criteria
+      if pick.constrained_by_age?(current_age)
+        Rails.logger.debug "Whoops!  Constrained by age."
+        next
+      end
+
+      Rails.logger.debug "Ok, not constrained by age"
+
+      # ignore card if it is already included
+      next if card_deck.include?(pick)
+
+      Rails.logger.debug "Adding card #{pick.id} #{pick.title}"
+
+      card_deck << pick
+
+      # remove picked card from our selection
+      cards = cards.select { |c| c!=pick }
+
+      current_age += DEFAULT_AGE_INCREMENT
+    end
+
+    card_deck
   end
 
   def constrained_by_age?(age)
